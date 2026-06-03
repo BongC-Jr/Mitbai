1976cf848f4bfb061460c49ec3a4597dcb75fcbf
BongCabebe <120769885+BongC-Jr@users.noreply.github.com>
2026-06-03 01:39:51 +0000

    03 June 2026 Eastwood BACJr - First commit

---
+++ discord_only.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+      await message.channel.send('Hi!')
+
+client.run(os.getenv('TOKEN'))
+
+
+    
